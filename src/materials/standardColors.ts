46f878a720c68654d6faa220ac1a317905508160
NorthCore Solutions <lukas.nagel@northcore-eu.de>
2026-08-04 13:16:14 +0200

    Standardfarben mit Namen ergänzen

--- src/materials/standardColors.ts
+++ src/materials/standardColors.ts
@@ -1,6 +1,32 @@
+export interface StandardColor {
+  name: string;
+  hex: string;
+}
+
 export const STANDARD_COLOR_COLUMNS = 7;
 
-export const STANDARD_FULL_COLORS = [
+const COLOR_NAMES = [
+  'Dunkelviolett', 'Dämmerungsblau', 'Tiefblau', 'Kräftiges Blau', 'Königsblau', 'Lavendelblau', 'Helles Lavendel',
+  'Marineblau-Violett', 'Aubergine', 'Traube', 'Violett', 'Lila', 'Zartes Violett', 'Helles Lila',
+  'Dunkle Pflaume', 'Tiefes Violett', 'Samtig Lila', 'Kräftiges Lila', 'Amethyst', 'Malve', 'Helllila',
+  'Dunkles Rubin', 'Pflaume', 'Helles Lila', 'Strahlendes Violett', 'Magenta', 'Blassviolett', 'Flieder',
+  'Tiefes Amethyst', 'Karminrot', 'Maulbeere', 'Helles Pink', 'Pink', 'Nelkenrosa', 'Zuckerwatte Pink',
+  'Tiefes Purpurrot', 'Kirschholz', 'Kirschrot', 'Tomatenrot', 'Erdbeerrosa', 'Helle Koralle', 'Rosenwasser',
+  'Kastanienbraun', 'Dunkelrot', 'Kirschrot', 'Knallrot', 'Korallenrot', 'Koralle', 'Pastellrosa',
+  'Rotbraun', 'Rostbraun', 'Korallenorange', 'Lachsfarben', 'Orange', 'Apricot', 'Pfirsich',
+  'Sattes Braun', 'Mittleres Braun', 'Dunkelorange', 'Warmes Orange', 'Pfirsich', 'Elfenbein', 'Vanille',
+  'Goldbraun', 'Sonnenscheingold', 'Sonnengelb', 'Helle Zitrone', 'Gelb', 'Löwenzahn', 'Hellgelb',
+  'Tiefes Moos', 'Moosgrün', 'Helles Khaki', 'Helles Grün', 'Limette', 'Helle Limette', 'Pastell-Limette',
+  'Tiefgrün', 'Waldgrün', 'Spritzige Limette', 'Grasgrün', 'Elektrogrün', 'Pastellgrün', 'Frühlingsnebel',
+  'Smaragdgrüne Nacht', 'Grüner Nebel', 'Grün', 'Meerschaum', 'Meeresbrise', 'Eiskalt', 'Eisiges Blaugrün',
+  'Tiefes Smaragdgrün', 'Tiefsee', 'Bergwiese', 'Zartes Blaugrün', 'Helle Jade', 'Gletscherblau', 'Hellblau',
+  'Evergreen', 'Tiefes Indigo', 'Tropisches Blaugrün', 'Kristall-Lagune', 'Türkisblau', 'Frostige Minze', 'Heller Himmel',
+  'Mitternachtsblaugrün', 'Tiefes Blaugrün', 'Dunkeltürkis', 'Aquablau', 'Strahlender Himmel', 'Aquamarinblau', 'Weißes Eis',
+  'Dunkles Ozeanblau', 'Dunkles Blaugrün', 'Helles Blau', 'Hellblau', 'Kristallblau', 'Puderblau', 'Mattierte Minze',
+  'Tiefsee', 'Meeresblau', 'Kobaltblau', 'Saphirblau', 'Zartblau', 'Eisblau', 'Blassblau'
+] as const;
+
+const COLOR_HEX_VALUES = [
   '#000B3D', '#00167A', '#0025CC', '#1F48FF', '#5271FF', '#99ACFF', '#C2CDFF',
   '#020C45', '#2C0A71', '#4910BC', '#5E17EB', '#8C52FF', '#BEA1F7', '#D8C7FA',
   '#200934', '#401268', '#6B1FAD', '#9440DD', '#B174E7', '#CEA8F0', '#E2CBF6',
@@ -21,15 +47,20 @@ export const STANDARD_FULL_COLORS = [
   '#001B3D', '#00357A', '#004AAD', '#1F80FF', '#5CA3FF', '#99C5FF', '#C2DCFF'
 ] as const;
 
-export const STANDARD_COLOR_PREVIEW = [
-  '#FFFFFF',
-  '#11161A',
-  '#FF3131',
-  '#FF751F',
-  '#FFD21F',
-  '#00BF63',
-  '#0CC0DF',
-  '#1F48FF',
-  '#8C52FF',
-  '#FF1FA9'
-] as const;
+export const STANDARD_FULL_COLORS: readonly StandardColor[] = COLOR_HEX_VALUES.map((hex, index) => ({
+  hex,
+  name: COLOR_NAMES[index] ?? hex
+}));
+
+export const STANDARD_COLOR_PREVIEW: readonly StandardColor[] = [
+  { name: 'Weiß', hex: '#FFFFFF' },
+  { name: 'Schwarz', hex: '#11161A' },
+  { name: 'Knallrot', hex: '#FF3131' },
+  { name: 'Lachsfarben', hex: '#FF751F' },
+  { name: 'Sonnengelb', hex: '#FFD21F' },
+  { name: 'Grün', hex: '#00BF63' },
+  { name: 'Aquablau', hex: '#0CC0DF' },
+  { name: 'Kräftiges Blau', hex: '#1F48FF' },
+  { name: 'Lila', hex: '#8C52FF' },
+  { name: 'Helles Pink', hex: '#FF1FA9' }
+];
