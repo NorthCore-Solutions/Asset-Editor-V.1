77eafa23f47be03f0b50eeb9c0a3dacac34b6fd3
NorthCore CI <actions@users.noreply.github.com>
2026-08-04 20:31:06 +0000

    Capacitor-Android-Grundgerüst hinzufügen

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.northcore.asseteditor',
+  appName: 'NorthCore Asset Editor',
+  webDir: 'dist'
+};
+
+export default config;
