6613d92c09c5872bf53ed653bb5de60126f33b21
NorthCore Solutions <lukas.nagel@northcore-eu.de>
2026-08-05 19:56:47 +0200

    Live-Update-Start aus dem Bootstrap entfernen

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
-import { initializeLiveUpdates } from './platform/liveUpdate';
 import './styles/global.css';
 
 const root = document.getElementById('root');
 if (!root) throw new Error('Root-Element fehlt.');
 createRoot(root).render(<StrictMode><App /></StrictMode>);
-void initializeLiveUpdates();
